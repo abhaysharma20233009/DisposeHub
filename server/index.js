e0522e9a1ff993c111106e029414b61ef97b963f
Abhay Sharma <abhaysharma21102006@gmail.com>
2025-04-18 22:07:10 +0530

    add leader board

--- server/index.js
+++ server/index.js
@@ -1,21 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
+// app.js
+import express from 'express';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
 
-dotenv.config();
-const app = express();        
+const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to Database
-connectDB();
+// Routes
+app.use('/api/users', userRoutes);
 
-// API Routes
-app.use("/api/users", userRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+// Export app
+export default app;
