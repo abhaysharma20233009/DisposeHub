92e777f9ced1e5e74d25b5161c107d61339675a7
AryanMishraJi <aryanmishraphysics@gmail.com>
2025-04-19 18:46:52 +0530

    added map features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,30 @@
-
-import { useState } from 'react'
 import 'leaflet/dist/leaflet.css';
 import './App.css'
-import LeafletMap from './components/LeafletMap';
+
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/register/Login";
 import Signup from "./pages/register/SignUp";
 import Leaderboard from './components/leaderboard';
+import Integrate from './pages/GeneralUser/Integrate';
+import DriverIntegrate from './pages/GeneralUser/driverIntegrate';
+//import RouteMap from './components/rasta';
 
 
 
 function App() {
+  const name = "Anshul";
+  const role = "driver"; 
 
   return (
 
     <Router>
       <Routes>
-        <Route path="/map" element={<LeafletMap />} />
+      <Route path="/map" element={<Integrate role={role} name={name} />} />
         <Route path="/login" element={ <Login />} />
         <Route path="/signup" element={ <Signup />} />
         <Route path="/leader-board" element={ <Leaderboard />} />
+        <Route path="/driver" element={ <DriverIntegrate role={role} name={name} />} />
+      
       </Routes>
     </Router>
 
